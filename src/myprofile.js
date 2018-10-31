0d2db894c45711a8280c0a1ddb670038848b66a1
RosemarieLopezH <rosemarie61@gmail.com>
2018-10-31 08:07:12 -0500

    cambio de nombre imagenes

--- src/myprofile.js
+++ src/myprofile.js
@@ -4,80 +4,95 @@ const handleError = () => {
   alert('Se ha presentado un error en la conexión con la data');
 }
 
-const getPerfil = (callback) => {
-  const perfilRequest = new XMLHttpRequest();
-  perfilRequest.open('GET', `myprofile.json`);
-  perfilRequest.onload = callback;
-  perfilRequest.onerror = handleError;
-  perfilRequest.send();
-}
+const typeProfile = localStorage.getItem('typeProfile')
 
-const showData = (data) => {
+// const getPerfil = (callback) => {
+//   const perfilRequest = new XMLHttpRequest();
+//   perfilRequest.open('GET', `myprofile.json`);
+//   perfilRequest.onload = callback;
+//   perfilRequest.onerror = handleError;
+//   perfilRequest.send();
+// }
+
+const showData = (data, result) => {
   const showPerfil = document.getElementById('container-data');
-  showPerfil.innerHTML = '';
-  Object.keys(data).forEach((id) => {
-    console.log(showData);
-    
-    const profile = data[id];
-    showPerfil.innerHTML +=
+  // showPerfil.innerHTML = '';
+  const arrProfiles = Object.keys(data);
+  arrProfiles.forEach(profile => {
+    // console.log(showData);
+    const showProfile = data[profile]
+    // const profile = ;
+    if (showProfile.key === result) {
+      console.log('hola')
+      showPerfil.innerHTML =
+        `
+        <h5 class=" text-center">${showProfile.title}</h5>
+          <div class="row" id=${showProfile.key}>
+            <div class="col-md-6">
+              <img src=${showProfile.stats} alt="image">
+              <img src=${showProfile.description} alt="image">
+            </div>
+            <div class="col-md-6"><img alt="description" src=${showProfile.image}></div> 
+          </div>
       `
-      <div class= "col 12" id=${id}>
-        <div class="col 12">${profile.title}</div>
-        <div class="col 12"><img with="400px" src=${profile.image} alt="image">>/div>
-        <div class="col 12">${profile.description}</div>    
-      `
+    }
   })
 }
 
-const selectProfile = () => {
-  getPerfil(() => {
-    const profileView = JSON.parse(event.currentTarget.responseText);    
-    const agresivo = document.getElementById('agresivo');
-    const moderado = document.getElementById('moderado');
-    const defensivo= document.getElementById('defensivo');
-    const conservador= document.getElementById('conservador');
-    const altaconservador = document.getElementById('altaconservador')
-    
-    agresivo.addEventListener('click', (e) => {
-      e.preventDefault();        
-      const filterProfile = profileView.filter((profile) => {
-        return profile.key === 'agresivo';
-      })
-      showData(filterProfile);
-    });
+fetch('./myprofile.json')
+  .then(res => res.json())
+  .then(data => showData(data, typeProfile))
+  .catch(err => console.error(err))
 
-    moderado.addEventListener('click', (e) => {
-      e.preventDefault();        
-      const filterProfile = profileView.filter((profile) => {
-        return profile.key === 'moderado';
-      })
-      showData(filterProfile);
-    });
+// const selectProfile = () => {
+//   getPerfil(() => {
+//     const profileView = JSON.parse(event.currentTarget.responseText);    
+//     const agresivo = document.getElementById('agresivo');
+//     const moderado = document.getElementById('moderado');
+//     const defensivo= document.getElementById('defensivo');
+//     const conservador= document.getElementById('conservador');
+//     const altaconservador = document.getElementById('altaconservador')
 
-    defensivo.addEventListener('click', (e) => {
-      e.preventDefault();        
-      const filterProfile = profileView.filter((profile) => {
-        return profile.key === 'defensivo';
-      })
-      showData(filterProfile);
-    });
+//     agresivo.addEventListener('click', (e) => {
+//       e.preventDefault();        
+//       const filterProfile = profileView.filter((profile) => {
+//         return profile.key === 'agresivo';
+//       })
+//       showData(filterProfile);
+//     });
 
-    conservador.addEventListener('click', (e) => {
-      e.preventDefault();        
-      const filterProfile = profileView.filter((profile) => {
-        return profile.key === 'conservador';
-      })
-      showData(filterProfile);
-    });
+//     moderado.addEventListener('click', (e) => {
+//       e.preventDefault();        
+//       const filterProfile = profileView.filter((profile) => {
+//         return profile.key === 'moderado';
+//       })
+//       showData(filterProfile);
+//     });
 
-    altaconservador.addEventListener('click', (e) => {
-      e.preventDefault();        
-      const filterProfile = profileView.filter((profile) => {
-        return profile.key === 'altaconservador';
-      })
-      showData(filterProfile);
-    });
-  })
-}
+//     defensivo.addEventListener('click', (e) => {
+//       e.preventDefault();        
+//       const filterProfile = profileView.filter((profile) => {
+//         return profile.key === 'defensivo';
+//       })
+//       showData(filterProfile);
+//     });
 
-selectProfile();
+//     conservador.addEventListener('click', (e) => {
+//       e.preventDefault();        
+//       const filterProfile = profileView.filter((profile) => {
+//         return profile.key === 'conservador';
+//       })
+//       showData(filterProfile);
+//     });
+
+//     altaconservador.addEventListener('click', (e) => {
+//       e.preventDefault();        
+//       const filterProfile = profileView.filter((profile) => {
+//         return profile.key === 'altaconservador';
+//       })
+//       showData(filterProfile);
+//     });
+//   })
+// }
+
+// selectProfile();
