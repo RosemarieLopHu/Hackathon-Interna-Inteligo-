af77ea535fd5578951549286578dffc7b37fdb05
RosemarieLopezH <rosemarie61@gmail.com>
2018-10-30 22:04:11 -0500

    agrega ruta img

--- src/miperfil.js
+++ src/miperfil.js
@@ -1,4 +1,4 @@
-
+const showPerfil = document.getElementById('container-data');
 
 const getProjects = (callback) => {
     const perfilRequest = new XMLHttpRequest();
@@ -7,4 +7,26 @@ const getProjects = (callback) => {
     perfilRequest.onerror = handleError;
     perfilRequest.send();
   }
+  
+  const showData = (data) => {
+    showPerfil.innerHTML = '';
+    Object.keys(data).forEach((id) => {
+      const project = data[id];
+      showProject.innerHTML += `
+      <div class="col s12 project" id=${id}>
+        <div class="col s12 title-project center">
+          ${project.title}
+        </div>
+        <div class="col s12 img-project center-align">
+          <img width="330px" src=${project.image} alt="imagen perfiles">
+        </div>
+        <div class="col s12 description">
+          ${project.description}
+        </div>
+       
+      </div> 
+      `  
+    })
+  }
+  console.log(showdata);
   
