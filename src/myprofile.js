d81d5d14f113e0f42d90d547d32c414cc98ba203
Geraldine13 <geral1312@hotmail.com>
2018-10-31 11:22:35 -0500

    ok

--- src/myprofile.js
+++ src/myprofile.js
@@ -6,11 +6,22 @@ const handleError = () => {
 
 const typeProfile = localStorage.getItem('typeProfile')
 
+// const getPerfil = (callback) => {
+//   const perfilRequest = new XMLHttpRequest();
+//   perfilRequest.open('GET', `myprofile.json`);
+//   perfilRequest.onload = callback;
+//   perfilRequest.onerror = handleError;
+//   perfilRequest.send();
+// }
+
 const showData = (data, result) => {
   const showPerfil = document.getElementById('container-data');
+  // showPerfil.innerHTML = '';
   const arrProfiles = Object.keys(data);
   arrProfiles.forEach(profile => {
+    // console.log(showData);
     const showProfile = data[profile]
+    // const profile = ;
     if (showProfile.key === result) {
       console.log('hola')
       showPerfil.innerHTML =
@@ -18,23 +29,12 @@ const showData = (data, result) => {
         <h5 class=" text-center">${showProfile.title}</h5>
           <div class="row" id=${showProfile.key}>
             <div class="col-md-6">
-              <div class="image-container">
-              <img alt="description" src=${showProfile.image} class="img-fluid" alt="Responsive image">
-              </div>
+              <img src=${showProfile.stats} alt="image">
+              <img src=${showProfile.description} alt="image">
             </div>
-            <div class="col-md-6">
-              <div class="image-container">
-              <img src=${showProfile.stats} alt="image" class="img-fluid" alt="Responsive image">
-              </div>
-            </div> 
-            <div class="col-md-6" >
-              <div class="text-container">
-              <p><sup>1</sup>&nbspSobre la base de rentabilidades de largo plazo esperadas para las categorías de activo que componen los portafolios de cada perfil.</p>                
-              <p><sup>2</sup>&nbspSobre la base de un back test aplicado a los perfiles para los últimos 10 años.</p>    
-              </div>            
-           </div>
+            <div class="col-md-6"><img alt="description" src=${showProfile.image}></div> 
           </div>
-        `
+      `
     }
   })
 }
@@ -43,3 +43,56 @@ fetch('./myprofile.json')
   .then(res => res.json())
   .then(data => showData(data, typeProfile))
   .catch(err => console.error(err))
+
+// const selectProfile = () => {
+//   getPerfil(() => {
+//     const profileView = JSON.parse(event.currentTarget.responseText);    
+//     const agresivo = document.getElementById('agresivo');
+//     const moderado = document.getElementById('moderado');
+//     const defensivo= document.getElementById('defensivo');
+//     const conservador= document.getElementById('conservador');
+//     const altaconservador = document.getElementById('altaconservador')
+
+//     agresivo.addEventListener('click', (e) => {
+//       e.preventDefault();        
+//       const filterProfile = profileView.filter((profile) => {
+//         return profile.key === 'agresivo';
+//       })
+//       showData(filterProfile);
+//     });
+
+//     moderado.addEventListener('click', (e) => {
+//       e.preventDefault();        
+//       const filterProfile = profileView.filter((profile) => {
+//         return profile.key === 'moderado';
+//       })
+//       showData(filterProfile);
+//     });
+
+//     defensivo.addEventListener('click', (e) => {
+//       e.preventDefault();        
+//       const filterProfile = profileView.filter((profile) => {
+//         return profile.key === 'defensivo';
+//       })
+//       showData(filterProfile);
+//     });
+
+//     conservador.addEventListener('click', (e) => {
+//       e.preventDefault();        
+//       const filterProfile = profileView.filter((profile) => {
+//         return profile.key === 'conservador';
+//       })
+//       showData(filterProfile);
+//     });
+
+//     altaconservador.addEventListener('click', (e) => {
+//       e.preventDefault();        
+//       const filterProfile = profileView.filter((profile) => {
+//         return profile.key === 'altaconservador';
+//       })
+//       showData(filterProfile);
+//     });
+//   })
+// }
+
+// selectProfile();
