fe27b4766de432ba62a777f79954b86574da980d
RosemarieLopezH <rosemarie61@gmail.com>
2018-10-31 01:29:34 -0500

    cambio de ruta profile.json

--- src/myprofile.js
+++ src/myprofile.js
@@ -1,18 +1,32 @@
-redirectToForm.addEventListener('click', () => location.href = './register.html');
-
+redirect.addEventListener('click', () => location.href = './register.html');
 
 const getPerfil = (callback) => {
-    const perfilRequest = new XMLHttpRequest();
-    perfilRequest.open('GET', `miperfil.json`);
-    perfilRequest.onload = callback;
-    perfilRequest.onerror = handleError;
-    perfilRequest.send();
-  }
-
-  const showPerfil=()=>{
-    const showPerfil = document.getElementById('container-data');
+  const perfilRequest = new XMLHttpRequest();
+  perfilRequest.open('GET', `miperfil.json`);
+  perfilRequest.onload = callback;
+  perfilRequest.onerror = handleError;
+  perfilRequest.send();
+}
 
 
-  }
-  
+const showData = (data) => {
+  const showPerfil = document.getElementById('container-data');
+  showPerfil.innerHTML = '';
+  Object.keys(data).forEach((id) => {
+    const profile = data[id];
+    showPerfil.innerHTML +=
+      `
+      <div class= "col 12" id=${id}>
+        <div class="col 12">${profile.title}</div>
+        <div class="col 12"><img with="400px" src=${profile.image} alt="image">>/div>
+        <div class="col 12">${profile.description}</div>    
+      `
+  })
+}
+const selectProject = () => {
+  getPerfil(()=> {
+    console.log('hola');
+    
 
+  })
+}
